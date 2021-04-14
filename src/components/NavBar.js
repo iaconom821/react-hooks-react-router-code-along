6dfdeddd647a7e8359df5592a449c76aea8d6d94
iaconom821 <iaconom821@gmail.com>
2021-04-14 16:30:41 -0400

    Finish Code Along

---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { NavLink } from 'react-router-dom'
+
+const linkStyles = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+function NavBar() {
+    return (
+      <div>
+        <NavLink
+          to="/"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkBlue",
+          }}>
+            Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkBlue",
+          }}>
+            About
+        </NavLink>
+        <NavLink
+          to="/login"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkBlue",
+          }}>
+            Login
+        </NavLink>
+      </div>
+    )
+  }
+
+  export default NavBar
